5eafac1c2bba5ef4d1118dbf9f85f9992fa265c2
BryanPortillo13 <pm18042@ues.edu.sv>
2024-04-19 19:54:35 -0600

    Datos de Configuracion del Contribuyente
    Codigo de Cornejo
    
    Co-Authored-By: Cornejo44 <80413712+Cornejo44@users.noreply.github.com>

---
+++ app/src/main/java/com/example/facturaelectronica/ConfDatosDelContribuyenteActivity.kt
@@ -0,0 +1,20 @@
+package com.example.facturaelectronica
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ConfDatosDelContribuyenteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_conf_datos_del_contribuyente)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
