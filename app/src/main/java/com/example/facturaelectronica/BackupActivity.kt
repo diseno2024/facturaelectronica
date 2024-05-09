9d40328986b039b543aabf3c9a39c75f2ee483d2
BryanPortillo13 <pm18042@ues.edu.sv>
2024-05-09 14:58:26 -0600

    GENERAR Y RESTAURAR RESPALDO BACKEN
    Co-Authored-By: ArmandoMtrz <141801062+ArmandoMtrz@users.noreply.github.com>
    Co-Authored-By: Bryan Biolante <71799175+bl19019@users.noreply.github.com>

--- app/src/main/java/com/example/facturaelectronica/BackupActivity.kt
+++ app/src/main/java/com/example/facturaelectronica/BackupActivity.kt
@@ -15,16 +15,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
+import android.os.Environment
+import android.util.Log
+import java.io.File
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
 
 
 class BackupActivity : AppCompatActivity() {
 
     private lateinit var buttonSelectTime: Button
     private lateinit var editTextSelectedTime: EditText
+    private val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_backup) // Asegúrate de que este sea el nombre correcto de tu layout
+        setContentView(R.layout.activity_backup)
 
         val switchEncendido = findViewById<Switch>(R.id.switchEncendido)
         val textEstado = findViewById<TextView>(R.id.textEstado)
@@ -37,31 +47,24 @@ class BackupActivity : AppCompatActivity() {
             }
         }
 
+        val btnRegistrar = findViewById<Button>(R.id.btnRegistrar)
+        btnRegistrar.setOnClickListener {
+            requestWriteExternalStoragePermission()
+        }
+
         val spinnerFrecuencia = findViewById<Spinner>(R.id.spinnerFrecuencia)
-
-        // Obtén las opciones del StringArray en strings.xml
         val opciones = resources.getStringArray(R.array.frecuencia_options)
-
-        // Crea un adaptador usando las opciones y un diseño predeterminado para los elementos del Spinner
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
-
-        // Especifica el diseño para los elementos desplegables
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        // Establece el adaptador en el Spinner
         spinnerFrecuencia.adapter = adapter
 
-        // Implementa un listener si necesitas manejar las selecciones del usuario
         spinnerFrecuencia.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                // Aquí puedes realizar acciones basadas en la opción seleccionada
                 val opcionSeleccionada = opciones[position]
-                // Por ejemplo, puedes mostrar un mensaje con la opción seleccionada
                 Toast.makeText(applicationContext, "Seleccionaste: $opcionSeleccionada", Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Implementa acciones si no se selecciona nada, si es necesario
             }
         }
 
@@ -95,19 +98,76 @@ class BackupActivity : AppCompatActivity() {
             finish()
         }
     }
+
     override fun onBackPressed() {
-        super.onBackPressed() // Llama al método onBackPressed() de la clase base
+        super.onBackPressed()
         val intent = Intent(this, MenuActivity::class.java)
         startActivity(intent)
         finish()
     }
+
+    private fun requestWriteExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                WRITE_EXTERNAL_STORAGE_REQUEST_CODE
+            )
+        } else {
+            createBackupFolder()
+        }
+    }
+
+    private fun createBackupFolder() {
+        // Obtener el directorio de documentos públicos
+        val parentDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+
+// Nombre del directorio de respaldo dentro del directorio de documentos
+        val backupDirectoryName = "respaldo_factura2024"
+
+// Crear el objeto File para el directorio de respaldo
+        val backupDirectory = File(parentDir, backupDirectoryName)
+
+// Verificar si el directorio ya existe
+        if (!backupDirectory.exists()) {
+            // Intentar crear el directorio si no existe
+            if (backupDirectory.mkdirs()) {
+                // El directorio se creó exitosamente
+                Log.d("BackupActivity", "Directorio de respaldo creado exitosamente en: ${backupDirectory.absolutePath}")
+                Toast.makeText(this, "Directorio de respaldo creado exitosamente", Toast.LENGTH_SHORT).show()
+            } else {
+                // Error al crear el directorio
+                Log.e("BackupActivity", "Error al crear el directorio de respaldo")
+                Toast.makeText(this, "Error al crear el directorio de respaldo", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            // El directorio ya existe
+            Log.d("BackupActivity", "El directorio de respaldo ya existe en: ${backupDirectory.absolutePath}")
+            Toast.makeText(this, "El directorio de respaldo ya existe", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                createBackupFolder()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Los permisos son necesarios para realizar el respaldo",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
--- app/src/main/java/com/example/facturaelectronica/RestauracionActivity.kt
+++ app/src/main/java/com/example/facturaelectronica/RestauracionActivity.kt
@@ -1,37 +1,103 @@
 package com.example.facturaelectronica
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import android.widget.Button
 
 class RestauracionActivity : AppCompatActivity() {
+
+    private val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_restauracion)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val btnAtras: ImageButton = findViewById(R.id.atras)
         btnAtras.setOnClickListener {
-            // Crear un intent para ir a MenuActivity
             val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
-            finish()  // Finalizar la actividad actual si se desea
+            finish()
+        }
+
+        val btnRealizarRecuperacion: Button = findViewById(R.id.buttonSelectTime)
+        btnRealizarRecuperacion.setOnClickListener {
+            requestWriteExternalStoragePermission()
         }
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed() // Llama al método onBackPressed() de la clase base
-        val intent = Intent(this, MenuActivity::class.java)
-        startActivity(intent)
-        finish()
+
+
+    private fun requestWriteExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                WRITE_EXTERNAL_STORAGE_REQUEST_CODE
+            )
+        } else {
+            createRestorationFolder()
+        }
+    }
+
+    private fun createRestorationFolder() {
+        val parentDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+        val backupDirectoryName = "respaldo_factura2024"
+        val restorationDirectoryName = "restauracion_factura"
+
+        val backupDirectory = File(parentDir, backupDirectoryName)
+        val restorationDirectory = File(backupDirectory, restorationDirectoryName)
+
+        if (backupDirectory.exists()) { // Verificar si la carpeta de respaldo existe
+            if (!restorationDirectory.exists()) { // Verificar si la carpeta de restauración no existe
+                if (restorationDirectory.mkdirs()) { // Intentar crear la carpeta de restauración
+                    Log.d("RestauracionActivity", "Directorio de restauración creado en: ${restorationDirectory.absolutePath}")
+                    Toast.makeText(this, "Directorio de restauración creado exitosamente", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.e("RestauracionActivity", "Error al crear el directorio de restauración")
+                    Toast.makeText(this, "Error al crear el directorio de restauración", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Log.d("RestauracionActivity", "El directorio de restauración ya existe en: ${restorationDirectory.absolutePath}")
+                Toast.makeText(this, "El directorio de restauración ya existe", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Log.e("RestauracionActivity", "La carpeta de respaldo no existe")
+            Toast.makeText(this, "La carpeta de respaldo no existe", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                createRestorationFolder()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Los permisos son necesarios para realizar la restauración",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
+
+
