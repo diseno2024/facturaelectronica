73c2bee391818d7ccf00c942c9c6f993877a72c5
Gabym03 <100321923+Gabym03@users.noreply.github.com>
2024-05-10 19:18:12 -0600

    modificaciones en el .kt de re_cliente

--- app/src/main/java/com/example/facturaelectronica/ReClienteActivity.kt
+++ app/src/main/java/com/example/facturaelectronica/ReClienteActivity.kt
@@ -1,18 +1,34 @@
 package com.example.facturaelectronica
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import android.widget.ImageButton
-import android.content.Intent
-import android.widget.Button
 import androidx.core.view.WindowInsetsCompat
+import com.couchbase.lite.CouchbaseLite
+import com.couchbase.lite.Database
+import com.couchbase.lite.MutableDocument
+import com.example.facturaelectronica.R
+
 
 class ReClienteActivity : AppCompatActivity() {
-    private lateinit var spinner1: Spinner
+    private lateinit var spinnerDep: Spinner
+    private lateinit var spinnerMun: Spinner
+    private lateinit var nombreEditText: EditText
+    private lateinit var nitEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var direccionEditText: EditText
+    private lateinit var departamentoSpinner: Spinner
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,27 +39,50 @@ class ReClienteActivity : AppCompatActivity() {
             insets
         }
         // Inicializa el Spinner
-        spinner1 = findViewById(R.id.contribuyente)
-        val opciones = arrayOf("Tipo de Contribuyente","Crédito Fiscal", "Consumidor Final")
+        spinnerDep = findViewById(R.id.departamento)
+        val departamentos = arrayOf("Ahuachapán", "Cabañas", "Chalatenango", "Cuscatlán",
+            "La Libertad","La paz", "La Unión", "Morazán", "San Miguel", "San Vicente", "Santa Ana",
+            "Sonsonate", "Usulután")
 
         // Configura el adaptador para el Spinner
-        val adapter = ArrayAdapter(this, R.layout.spinner_personalizado, opciones)
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_per)
-        spinner1.adapter = adapter
+        val adapterDep = ArrayAdapter(this, R.layout.spinner_personalizado, departamentos)
+        adapterDep.setDropDownViewResource(R.layout.spinner_dropdown_per)
+        spinnerDep.adapter = adapterDep
 
-        val btnCancelar: Button = findViewById(R.id.btnCancelar)
-        btnCancelar.setOnClickListener {
-            // Crear un intent para ir a MenuActivity
+        // Inicializa el Spinner
+        spinnerMun = findViewById(R.id.municipio)
+        val municipios = arrayOf("Municipios", "de", "cada", "departamento")
+
+        // Configura el adaptador para el Spinner
+        val adapterMun = ArrayAdapter(this, R.layout.spinner_personalizado, municipios)
+        adapterMun.setDropDownViewResource(R.layout.spinner_dropdown_per)
+        spinnerMun.adapter = adapterMun
+
+        val botonAgregar = findViewById<Button>(R.id.btnAgregar) // Reemplaza con el ID real de tu botón
+        botonAgregar.setOnClickListener {
             val intent = Intent(this, EmitirCFActivity::class.java)
             startActivity(intent)
-            finish()  // Finalizar la actividad actual si se desea
         }
+        val btnCancelar = findViewById<Button>(R.id.btnCancelar) // Reemplaza con el ID real de tu botón
+        btnCancelar.setOnClickListener {
+            val intent = Intent(this, EmitirCFActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        // Inicializa las vistas
+        nombreEditText = findViewById(R.id.nombre)
+        nitEditText = findViewById(R.id.nit)
+        emailEditText = findViewById(R.id.email)
+        direccionEditText = findViewById(R.id.direccion)
+        departamentoSpinner = findViewById(R.id.departamento)
+
+
+
+
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed() // Llama al método onBackPressed() de la clase base
-        val intent = Intent(this, EmitirCFActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
+
+
+
 }
