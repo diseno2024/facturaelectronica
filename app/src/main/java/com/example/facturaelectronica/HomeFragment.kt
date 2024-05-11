2d9feba74f2adf3773ff0fa852fa09755de6adbf
Gochezzz <gp21039@ues.edu.sv>
2024-05-10 21:41:27 -0600

    color del boton

--- app/src/main/java/com/example/facturaelectronica/HomeFragment.kt
+++ app/src/main/java/com/example/facturaelectronica/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.facturaelectronica
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
 
@@ -22,19 +25,30 @@ class HomeFragment : Fragment() {
 
         // Maneja la navegación a través de BottomNavigationView
         val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.boton_navegacion)
+        val floatingActionButton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
         bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.boton_factura -> {
+                    floatingActionButton.backgroundTintList = ColorStateList.valueOf(hexToColorInt("#80BFA8"))
                     (activity as MenuActivity).navigateToFragment(FacturaFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.boton_creditoFiscal -> {
+                    floatingActionButton.backgroundTintList = ColorStateList.valueOf(hexToColorInt("#892026"))
                     (activity as MenuActivity).navigateToFragment(CFiscalFragment())
                     return@setOnNavigationItemSelectedListener true
                 }
                 // Agrega más casos según sea necesario para más fragmentos
                 else -> return@setOnNavigationItemSelectedListener false
             }
+        }
+    }
+    fun hexToColorInt(hex: String): Int {
+        return try {
+            Color.parseColor(hex)
+        } catch (e: IllegalArgumentException) {
+            // Manejar errores si el formato del color hexadecimal es incorrecto
+            Color.BLACK // Valor predeterminado en caso de error
         }
     }
 }
