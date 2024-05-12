6741a63bba0728df742d94f14daa6f10bc6263c0
EdMendez00 <mm19247@ues.edu.sv>
2024-05-11 23:03:01 -0600

    Modifica infoReceptoresActivity para obtener datos. #24

--- app/src/main/java/com/example/facturaelectronica/InfoReceptoresActivity.kt
+++ app/src/main/java/com/example/facturaelectronica/InfoReceptoresActivity.kt
@@ -2,6 +2,8 @@ package com.example.facturaelectronica
 import android.os.Bundle
 import android.widget.ImageButton
 import android.content.Intent
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class InfoReceptoresActivity : AppCompatActivity() {
@@ -10,18 +12,34 @@ class InfoReceptoresActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_receptores)
 
+        val linearLayout = findViewById<LinearLayout>(R.id.containerLayout)
+        val dataList = obtenerDatosGuardados()
+
+        dataList.forEach { data ->
+            val itemLayout = layoutInflater.inflate(R.layout.layout_list_item_contribuyentes, null)
+
+            val textViewNombreComercial = itemLayout.findViewById<TextView>(R.id.textViewNombreComercial)
+            val textViewCorreo = itemLayout.findViewById<TextView>(R.id.textViewCorreo)
+            val textViewTelefono = itemLayout.findViewById<TextView>(R.id.textViewTelefono)
+            val textViewNIT = itemLayout.findViewById<TextView>(R.id.textViewNIT)
+            val textViewDUI = itemLayout.findViewById<TextView>(R.id.textViewDUI)
+
+            val datos = data.split("\n")
+            textViewNombreComercial.text = datos[0]
+            textViewCorreo.text = datos[1]
+            textViewTelefono.text = datos[2]
+            textViewNIT.text = datos[3]
+            textViewDUI.text = datos[4]
+            linearLayout.addView(itemLayout)
+        }
 
         val botonAtras = findViewById<ImageButton>(R.id.BotonAtras)
-
-        // Configura un OnClickListener para el botón
         botonAtras.setOnClickListener {
             // Crea un Intent para iniciar la actividad Comprobantecf
             val intent = Intent(this, Comprobantecf::class.java)
             startActivity(intent) // Inicia la actividad Comprobantecf
             finish()
         }
-
-
     }
     override fun onBackPressed() {
         super.onBackPressed() // Llama al método onBackPressed() de la clase base
