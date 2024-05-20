124425ac05e877aea6f22440a8c32b3c7fb99017
Gabym03 <100321923+Gabym03@users.noreply.github.com>
2024-05-19 22:12:55 -0600

    Agregando spinner, modificaciones en front, y agregando back para spinner en re_datos_contribuyente

--- app/src/main/java/com/example/facturaelectronica/ReDatosContribuyenteActivity.kt
+++ app/src/main/java/com/example/facturaelectronica/ReDatosContribuyenteActivity.kt
@@ -1,8 +1,14 @@
 package com.example.facturaelectronica
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -27,6 +33,65 @@ class ReDatosContribuyenteActivity : AppCompatActivity() {
     private lateinit var email: EditText
     private lateinit var nombreComercial: EditText
     private lateinit var telefono: EditText
+    private lateinit var departamento: Spinner
+    private lateinit var municipio: Spinner
+    private lateinit var spinnerDep: Spinner
+    private lateinit var spinnerMun: Spinner
+
+    private val municipiosMap = mapOf(
+        "Ahuachapán" to listOf("Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco",
+            "El Refugio", "Guaymango", "Jujutla", "San Francisco Menéndez", "San Lorenzo",
+            "San Pedro Puxtla", "Tacuba", "Turín"),
+        "Santa Ana" to listOf("Candelaria de la Frontera","Coatepeque","Chalchuapa","El Congo",
+            "El Porvenir", "Masahuat","Metapán","San Antonio Pajonal","San Sebastián Salitrillo",
+            "Santa Ana","Sta Rosa Guachipilin","Stgo de la Frontera","Texistepeque"),
+        "Sonsonate" to listOf("Acajutla","Armenia","Caluco","Cusinahuat","Sta Ishuatan","Izalco",
+            "Juayúa","Nahuizalco", "Nahuilingo","Salcoatitan","San Antonio del Monte","San Julián",
+            "Sta C Masahuat","Santo Domingo Gúzman", "Sonsonate","Sonzacate"),
+        "Chalatenango" to listOf("Agua Caliente","Arcatao","Azacualpa","Citalá","Comalapa",
+            "Concepción Quezaltepeque","Chalatenango","Dulce Nombre de María","El Carrizal", "EL paraíso",
+            "La Laguna","La Palma","La Reina","Las Vueltas", "Nombre de Jesus","Nueva Concepción",
+            "Nueva Trinidad","Ojos de Agua","Potonico","San Antonio de La Cruz","San Ant Ranchos",
+            "San Fernando","San Francisco Lempa","San Francisco Morazán","San Ingnacio","San I Labrador",
+            "San J Cancasque", "San Jose Flores","San Luis Carmen","San Mig Mercedes","San Rafael",
+            "Santa Rita", "Tejutla"),
+        "La Libertad" to listOf("Antiguo Cuscatlán", "Ciudad Arce","Colón","Comasagua", "Chiltupan",
+            "Huizúcar","Jayaque","Jicalapa","La Libertad","Nuevo Cuscatlán","Santa Tecla","Quezaltepeque",
+            "Sacacoyo","San J Villanueva","San Juan Opico","San P Tacachico","Tamanique","Talnique",
+            "Teotepeque","Tepecoyo","Zaragoza"),
+        "San Salvador" to listOf("Aguilares","Apopa","Ayutuxtepeque","Cuscatancingo","El Paisnal",
+            "Guazapa","Ilopango","Mejicanos","Nejapa","Panchimalco","Rosario de Mora","San Marcos",
+            "San Martin","San Salvador","Stg Texacuangos","Santo Tomas","Soyapango","Tonacatepeque",
+            "Ciudad Delgado"),
+        "Cuscatlan" to listOf("Candelaria","Cojutepeque","El Carmen","El Rosario","Monte San Juan",
+            "Orat Concepción","San B PeruliapA","San Cristóbal","San J Guayabal","San P Perulapán",
+            "San Raf Cedros","San Ramón","Sta C Analquito","Sta C Michapa","Suchitoto","Tenancingo"),
+        "La Paz" to listOf("Cuyultitán","El Rosario","Jesuralén","Merced La Ceiba","Olocuilta",
+            "Paraíso Osorio", "San Ant Masahuat","San Emigdio","San Fco Chinamec","San J Nonualco",
+            "San Juan Talpa","Sam Juan Tepezontes","San Juan Luis Talpa","San Miguel Tepezontes",
+            "San pedro Masahuat","San Pedro Nonualco","San R Obrajuela","Sta Ma Ostuma","Stgo Nonualco",
+            "Tapalhuaca","Zacatecoluca","San Luis La Herr","Cinquera", "Guacotecti"),
+        "Cabañas" to listOf("Cinquera","Guacotecti","Ilobasco","Jutiapa","San Isidro","Sensuntepeque",
+            "Tejutepeque","Victoria","Dolores"),
+        "San Vicente" to listOf("Apastepeque","Guadalupe","San Cay Istepeq","Santa Clara",
+            "Santo Domingo","SN EST Cartarina","San Ildefonso","San Lorenzo", "San Sebastián",
+            "San Vicente","Tecoluca","Tepetitán","Verapaz"),
+        "Usulatán" to listOf("Alegría","Berlín","California","Concep Batres","El Triunfo","Ereguayquín",
+            "Estanzuelas","Jiquilisco","Jucuapa","Jucuarán","Mercedes Umaña","Nueva Granada",
+            "Ozatlán","Puerto El Triunfo","San Agustín","SN Buenaventura","San Dionisio","Santa Elena",
+            "San FCO Javier","Santa María","Santiago De María","Tecapán","Usulután"),
+        "San Miguel" to listOf("Carolina","Ciudad Barrios","Comacarán","Chapeltique","Chinameca",
+            "Chirilagua","El Transito","Lolotique","Moncagua","Nueva Guadalupe","Nuevo Edén San Juan",
+            "Quelepa","San Antonio De Mosco","San Gerardo","San Jorge","San Luis Reina","San Miguel",
+            "San Rafael Oriente","Sesori","Uluazapa"),
+        "Morazán" to listOf("Arambala","Cacaopera","Corinto","Chilanga","Delic De Concep","El Divisadero","El Rosario",
+            "Gualococti","Guatajiagua","Joateca","Jocoaitique","Jocoro","Lolotiquillo","Meanguera",
+            "Osicala","Perquín","San Carlos","San Fernando","San Francisco Gotera","San Isidro",
+            "San Simón","Sensembra","Sociedad","Torola","Yamabal","Yoloaiquín"),
+        "La Unión" to listOf("Anamoros","Bolívar","Concep De OTE","Conchagua","El Carmen","El Sauce",
+            "Intipucá","La Unión","Lislique","Meang Del Golfo","Nueva Esparta","Pasaquina","Polorós",
+            "San Alejo","San Jose","Santa Rosa Lima","Yayantique","Yucuaiquín")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +107,6 @@ class ReDatosContribuyenteActivity : AppCompatActivity() {
         datosContribuyente = app.database
 
         val btnGuardar = findViewById<android.widget.Button>(R.id.guardarButton)
-        val btnCancelar = findViewById<android.widget.Button>(R.id.cancelarButton)
 
         // Inicializar las vistas
         razonSocial = findViewById(R.id.RazonSocialText)
@@ -53,15 +117,122 @@ class ReDatosContribuyenteActivity : AppCompatActivity() {
         email = findViewById(R.id.correoText)
         nombreComercial = findViewById(R.id.NomComercialText)
         telefono = findViewById(R.id.TelefonoText)
+        departamento = findViewById(R.id.departamento)
+        municipio = findViewById(R.id.municipio)
+
+        // Agregar TextWatcher para el campo de teléfono
+        telefono.addTextChangedListener(object : TextWatcher {
+            private var isUpdating = false
+            private val mask = "####-####"
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if (isUpdating) return
+
+                isUpdating = true
+                val formatted = formatPhoneNumber(s.toString())
+                telefono.setText(formatted)
+                telefono.setSelection(formatted.length)
+                isUpdating = false
+            }
+
+            private fun formatPhoneNumber(phone: String): String {
+                val digits = phone.replace(Regex("\\D"), "")
+                val formatted = StringBuilder()
+
+                var i = 0
+                for (m in mask.toCharArray()) {
+                    if (m != '#') {
+                        formatted.append(m)
+                        continue
+                    }
+                    if (i >= digits.length) break
+                    formatted.append(digits[i])
+                    i++
+                }
+                return formatted.toString()
+            }
+
+        })
+        nit.addTextChangedListener(object : TextWatcher {
+            private var isUpdating = false
+            private val mask = "####-######-###-#" // La máscara del NIT
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if (isUpdating) return
+
+                isUpdating = true
+                val formatted = formatNIT(s.toString())
+                nit.setText(formatted)
+                nit.setSelection(formatted.length)
+                isUpdating = false
+            }
+
+            private fun formatNIT(nit: String): String {
+                // Eliminar todos los caracteres no numéricos del NIT
+                val digits = nit.replace(Regex("\\D"), "")
+                val formatted = StringBuilder()
+
+                var i = 0
+                for (m in mask.toCharArray()) {
+                    if (m != '#') {
+                        formatted.append(m)
+                        continue
+                    }
+                    if (i >= digits.length) break
+                    formatted.append(digits[i])
+                    i++
+                }
+                return formatted.toString()
+            }
+        })
+
+        // Inicializa el Spinner de departamento
+        spinnerDep = findViewById(R.id.departamento)
+        val departamentos = arrayOf("Ahuachapán", "Santa Ana", "Sonsonate", "Chalatenango", "La Libertad",
+            "San Salvador", "Cuscatlan", "La Paz", "Cabañas", "San Vicente", "Usulután", "San Miguel", "Morazán","La Unión")
+
+        // Configura el adaptador para el Spinner de departamento
+        val adapterDep = ArrayAdapter(this, R.layout.spinner_personalizado, departamentos)
+        adapterDep.setDropDownViewResource(R.layout.spinner_dropdown_per)
+        spinnerDep.adapter = adapterDep
+
+        // Inicializa el Spinner de municipio
+        spinnerMun = findViewById(R.id.municipio)
+        val initialMunicipios = arrayOf("Seleccione un municipio")
+
+        // Configura el adaptador para el Spinner de municipio
+        val adapterMun = ArrayAdapter(this, R.layout.spinner_personalizado, initialMunicipios)
+        adapterMun.setDropDownViewResource(R.layout.spinner_dropdown_per)
+        spinnerMun.adapter = adapterMun
+
+        // Agrega un listener al Spinner de departamento
+        spinnerDep.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedDept = parent.getItemAtPosition(position).toString()
+                val municipios = municipiosMap[selectedDept] ?: listOf("Seleccione un municipio")
+                val adapterMun = ArrayAdapter(this@ReDatosContribuyenteActivity, R.layout.spinner_personalizado, municipios)
+                adapterMun.setDropDownViewResource(R.layout.spinner_dropdown_per)
+                spinnerMun.adapter = adapterMun
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // No hacer nada
+            }
+        }
+
         btnGuardar.setOnClickListener{
             guardarDatosContribuyente()
         }
 
-        btnCancelar.setOnClickListener{
-            val intent = Intent(this, MenuActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+
     }
 
     override fun onBackPressed() {
@@ -85,6 +256,8 @@ class ReDatosContribuyenteActivity : AppCompatActivity() {
         val emailText = email.text.toString()
         val nombreComercialText = nombreComercial.text.toString()
         val telefonoText = telefono.text.toString().replace("-", "")
+        val departamentoText = departamento.selectedItem.toString()
+        val municipioText = municipio.selectedItem.toString()
 
         // Crear un documento mutable para guardar en la base de datos
         val document = MutableDocument()
@@ -97,6 +270,8 @@ class ReDatosContribuyenteActivity : AppCompatActivity() {
             .setString("NombreComercial", nombreComercialText)
             .setString("Telefono", telefonoText)
             .setString("tipo", "contribuyente")
+            .setString("departamento", departamentoText)
+            .setString("municipio", municipioText)
 
         try {
             // Guardar el documento en la base de datos
