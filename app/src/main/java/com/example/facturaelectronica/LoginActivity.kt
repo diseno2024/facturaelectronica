202aec11f1758a8ae4b65f72b064c42fe2e152c6
LUisRT21 <luisisrael21r@gmail.com>
2024-05-19 21:55:19 -0600

    adicion-del-logo

--- app/src/main/java/com/example/facturaelectronica/LoginActivity.kt
+++ app/src/main/java/com/example/facturaelectronica/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.example.facturaelectronica
 
-
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
@@ -15,6 +21,7 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var pinEditText: EditText
     private lateinit var loginButton: Button
+    private lateinit var linkTextView: TextView
 
     private val DEFAULT_PIN = "123456" // PIN predeterminado
     private val PIN_FILE_NAME = "pins.json" // Nombre del archivo para guardar los pines
@@ -25,6 +32,7 @@ class LoginActivity : AppCompatActivity() {
 
         pinEditText = findViewById(R.id.pinEditText)
         loginButton = findViewById(R.id.loginButton)
+        linkTextView = findViewById(R.id.linkTextView)
 
         loginButton.setOnClickListener {
             val enteredPin = pinEditText.text.toString()
@@ -40,6 +48,8 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this@LoginActivity, "PIN incorrecto", Toast.LENGTH_SHORT).show()
             }
         }
+
+        setupClickableLink()
     }
 
     private fun loadPinsFromMenuActivity(): MutableList<String> {
@@ -51,5 +61,25 @@ class LoginActivity : AppCompatActivity() {
             mutableListOf()
         }
     }
+
+    private fun setupClickableLink() {
+        val text = "Leer mas sobre nosotros aquí"
+        val spannableString = SpannableString(text)
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val url = "https://stunning-dragon-ef975a.netlify.app"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            }
+        }
+
+        val start = text.indexOf("aquí")
+        val end = start + "aquí".length
+        spannableString.setSpan(clickableSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        linkTextView.text = spannableString
+        linkTextView.movementMethod = LinkMovementMethod.getInstance()
+    }
 }
+
 
