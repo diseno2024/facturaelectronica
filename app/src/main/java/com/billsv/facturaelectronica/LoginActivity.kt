b4b19fedff07ce2497223c3835643e799a9e46b1
EmersonOrellana <ot20006@ues.edu.sv>
2024-05-23 21:52:12 -0600

    Modificacion del login y home

--- app/src/main/java/com/billsv/facturaelectronica/LoginActivity.kt
+++ app/src/main/java/com/billsv/facturaelectronica/LoginActivity.kt
@@ -1,12 +1,14 @@
 package com.billsv.facturaelectronica
-
 import android.Manifest
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.text.InputFilter
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.method.LinkMovementMethod
@@ -16,6 +18,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -28,7 +31,6 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var loginButton: Button
     private lateinit var linkTextView: TextView
 
-    private val DEFAULT_PIN = "123456" // PIN predeterminado
     private val PIN_FILE_NAME = "pins.json" // Nombre del archivo para guardar los pines
     private val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 101
     private val permissionList: List<String> = if (Build.VERSION.SDK_INT >= 33) {
@@ -52,15 +54,29 @@ class LoginActivity : AppCompatActivity() {
         loginButton = findViewById(R.id.loginButton)
         linkTextView = findViewById(R.id.linkTextView)
 
+        // Verificar si es la primera vez que se instala la aplicación
+        val sharedPreferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        val isFirstTime = sharedPreferences.getBoolean("is_first_time", true)
+
+        if (isFirstTime) {
+            // Mostrar un cuadro de diálogo para crear un nuevo PIN
+            showCreatePinDialog()
+        } else {
+            // Verificar si hay un PIN guardado
+            val savedPins = loadPinsFromMenuActivity()
+            if (savedPins.isEmpty()) {
+                // Si no hay PIN guardado, mostrar mensaje para crear uno
+                Toast.makeText(this@LoginActivity, "Por favor, crea un nuevo PIN", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         loginButton.setOnClickListener {
             val enteredPin = pinEditText.text.toString()
             val savedPins = loadPinsFromMenuActivity()
-
-            if (enteredPin == DEFAULT_PIN || savedPins.contains(enteredPin)) {
+            if (savedPins.contains(enteredPin)) {
                 // PIN correcto, iniciar MenuActivity
                 val intent = Intent(this@LoginActivity, MenuActivity::class.java)
                 startActivity(intent)
-                requestPermissions()
                 finish() // Finalizar LoginActivity para evitar que el usuario regrese presionando el botón Atrás
             } else {
                 // PIN incorrecto, mostrar mensaje de error
@@ -71,6 +87,31 @@ class LoginActivity : AppCompatActivity() {
         setupClickableLink()
     }
 
+    private fun showCreatePinDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle("Crear PIN")
+        dialogBuilder.setMessage("Por favor, crea un nuevo PIN (máximo 6 dígitos)")
+        val input = EditText(this)
+        input.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(6)) // Máximo 6 dígitos
+        input.inputType = android.text.InputType.TYPE_CLASS_NUMBER // Solo números
+        dialogBuilder.setView(input)
+
+        dialogBuilder.setPositiveButton("Guardar", DialogInterface.OnClickListener { dialog, which ->
+            val enteredPin = input.text.toString()
+            savePin(enteredPin)
+            Toast.makeText(this@LoginActivity, "PIN creado correctamente", Toast.LENGTH_SHORT).show()
+
+            // Guardar que ya no es la primera vez que se instala la aplicación
+            val sharedPreferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("is_first_time", false)
+            editor.apply()
+        })
+
+        dialogBuilder.setCancelable(false)
+        dialogBuilder.show()
+    }
+
     private fun loadPinsFromMenuActivity(): MutableList<String> {
         val sharedPreferences = getSharedPreferences("pins_prefs", Context.MODE_PRIVATE)
         val pinsString = sharedPreferences.getString("pins", null)
@@ -79,6 +120,16 @@ class LoginActivity : AppCompatActivity() {
         } else {
             mutableListOf()
         }
+    }
+
+    private fun savePin(pin: String) {
+        val sharedPreferences = getSharedPreferences("pins_prefs", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        val savedPins = loadPinsFromMenuActivity()
+        savedPins.add(pin)
+        val pinsString = Gson().toJson(savedPins)
+        editor.putString("pins", pinsString)
+        editor.apply()
     }
 
     private fun setupClickableLink() {
@@ -99,6 +150,7 @@ class LoginActivity : AppCompatActivity() {
         linkTextView.text = spannableString
         linkTextView.movementMethod = LinkMovementMethod.getInstance()
     }
+
     private fun requestPermissions() {
         val permissionsToRequest = mutableListOf<String>()
 
@@ -120,6 +172,7 @@ class LoginActivity : AppCompatActivity() {
             )
         }
     }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -143,5 +196,3 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 }
-
-
