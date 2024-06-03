f3d5d6ebf95872d3a3c6d518c951e9058bbfc8f1
Gochezzz <gp21039@ues.edu.sv>
2024-06-02 23:35:27 -0600

    Articulos parte1

--- app/src/main/java/com/billsv/facturaelectronica/DescripcionActivity.kt
+++ app/src/main/java/com/billsv/facturaelectronica/DescripcionActivity.kt
@@ -3,21 +3,27 @@ package com.billsv.facturaelectronica
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.billsv.facturaelectronica.R
+import com.couchbase.lite.CouchbaseLiteException
+import com.couchbase.lite.MutableDocument
 
 class DescripcionActivity : AppCompatActivity() {
     private lateinit var Cantidad: EditText
     private lateinit var Producto: EditText
     private lateinit var Precio: EditText
     private lateinit var Tipo: Spinner
+    private lateinit var Unidad: Spinner
+    private lateinit var TipoV: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -39,7 +45,7 @@ class DescripcionActivity : AppCompatActivity() {
             // Apply the adapter to the spinner.
             Tipo.adapter = adapter
         }
-        val spinnerMedida: Spinner = findViewById(R.id.UnidadMedidaS)
+        Unidad = findViewById(R.id.UnidadMedidaS)
 // Create an ArrayAdapter using the string array and a default spinner layout.
         ArrayAdapter.createFromResource(
             this,
@@ -49,9 +55,9 @@ class DescripcionActivity : AppCompatActivity() {
             // Specify the layout to use when the list of choices appears.
             adapter.setDropDownViewResource(R.layout.spinner_dropdown_per)
             // Apply the adapter to the spinner.
-            spinnerMedida.adapter = adapter
+            Unidad.adapter = adapter
         }
-        val spinnerTventa: Spinner = findViewById(R.id.Tipo_Venta)
+        TipoV = findViewById(R.id.Tipo_Venta)
 // Create an ArrayAdapter using the string array and a default spinner layout.
         ArrayAdapter.createFromResource(
             this,
@@ -61,7 +67,7 @@ class DescripcionActivity : AppCompatActivity() {
             // Specify the layout to use when the list of choices appears.
             adapter.setDropDownViewResource(R.layout.spinner_dropdown_per)
             // Apply the adapter to the spinner.
-            spinnerTventa.adapter = adapter
+            TipoV.adapter = adapter
         }
         val Cancelar: Button = findViewById(R.id.Cancelar)
         Cancelar.setOnClickListener {
@@ -104,7 +110,48 @@ class DescripcionActivity : AppCompatActivity() {
     }
 
     private fun guardarItem() {
-        
+        val app = application as MyApp
+        val database = app.database
+        val tipo = Tipo.selectedItem.toString()
+        val cantidad = Cantidad.text.toString()
+        val UnidadMe = Unidad.selectedItem.toString()
+        val producto = Producto.text.toString()
+        val TipoV = TipoV.selectedItem.toString()
+        val Precio = Precio.text.toString()
+
+        // Crear un documento mutable para guardar en la base de datos
+        if (tipo != "" && cantidad != "") {
+            // Crear un documento mutable para guardar en la base de datos
+            val document = MutableDocument()
+                .setString("Tipod", tipo)
+                .setString("Cantidad", cantidad)
+                .setString("Unidad", UnidadMe)
+                .setString("Producto", producto)
+                .setString("Tipo de Venta", TipoV)
+                .setString("Precio", Precio)
+                .setString("tipo", "Articulo")
+
+            try {
+                // Guardar el documento en la base de datos
+                database.save(document)
+                Log.d("Descripcion", "Datos guardados correctamente: \n $document")
+                Toast.makeText(this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show()
+            } catch (e: CouchbaseLiteException) {
+                Log.e(
+                    "Descripcion",
+                    "Error al guardar los datos en la base de datos: ${e.message}",
+                    e
+                )
+                Toast.makeText(this, "Error al guardar los datos", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(
+                this,
+                "Error: Código de departamento o municipio no válido",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 
     override fun onBackPressed() {
