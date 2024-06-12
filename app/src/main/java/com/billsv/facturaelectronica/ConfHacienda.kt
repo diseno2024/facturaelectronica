7605a9204fdf66a1d2c3963dda657137706ca887
Gochezzz <gp21039@ues.edu.sv>
2024-06-11 22:55:02 -0600

    ingreso de credenciales

--- app/src/main/java/com/billsv/facturaelectronica/ConfHacienda.kt
+++ app/src/main/java/com/billsv/facturaelectronica/ConfHacienda.kt
@@ -1,18 +1,33 @@
 package com.billsv.facturaelectronica
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TableLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.couchbase.lite.CouchbaseLiteException
+import com.couchbase.lite.DataSource
+import com.couchbase.lite.Expression
+import com.couchbase.lite.Meta
+import com.couchbase.lite.MutableDocument
+import com.couchbase.lite.QueryBuilder
+import com.couchbase.lite.SelectResult
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 import java.util.UUID
 
 class ConfHacienda : AppCompatActivity() {
+    private lateinit var usuario: EditText
+    private lateinit var contraseña: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,22 +37,180 @@ class ConfHacienda : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val uuid: TextView = findViewById(R.id.textView55)
+        verificar()
+        val credenciales = obtenerDatosGuardados()
+        var usuarioapi = ""
+        var contraseñaapi = ""
+        credenciales.forEach { data ->
+            val datosapi = data.split("\n")
+            usuarioapi = datosapi[0]
+            contraseñaapi = datosapi[1]
+        }
+
+       /* val uuid: TextView = findViewById(R.id.textView55)
         val uuid2: TextView = findViewById(R.id.textView57)
         val calendar = Calendar.getInstance()
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         val timeFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
         // Formatea la fecha y la hora
         val formattedDate = dateFormat.format(calendar.time)
-        uuid.text = formattedDate
+        //uuid.text = formattedDate
         val formattedTime = timeFormat.format(calendar.time)
-        uuid2.text = formattedTime
+        //uuid2.text = formattedTime*/
+
+        usuario = findViewById(R.id.usuario)
+        contraseña = findViewById(R.id.contraseña)
+
         //https://apitest.dtes.mh.gob.sv/seguridad/auth
         //https://api.dtes.mh.gob.sv/seguridad/authw
+        val boton: Button = findViewById(R.id.button2)
+        boton.setOnClickListener {
+            guardarInformacion()
+            recreate()
+        }
+        val boton2: Button = findViewById(R.id.button3)
+        boton2.setOnClickListener {
+            habilitaredicion()
+        }
     }
-    private fun generarUUIDv4(): String {
+
+    private fun verificar():Boolean {
+        val datos = obtenerDatosGuardados()
+        datos.forEach { data ->
+            //sacar la data
+            val dato = data.split("\n")
+            val usuario = dato[0]
+            if(usuario!=""){
+                mostrardata(datos)
+                val boton: Button = findViewById(R.id.button2)
+                boton.visibility = View.GONE
+                val boton2: Button = findViewById(R.id.button3)
+                boton2.visibility = View.VISIBLE
+                return true
+            }
+
+        }
+        return false
+    }
+
+    private fun mostrardata(dataList: List<String>) {
+        //buscar los edittext
+        val usuario: EditText = findViewById(R.id.usuario)
+        val contraseña: EditText = findViewById(R.id.contraseña)
+        dataList.forEach { data ->
+            //sacar la data
+            val dato = data.split("\n")
+            val usuariod = dato[0]
+            val contraseñad = dato[1]
+
+            //pasar la data a los edittext
+            usuario.setText(usuariod)
+            contraseña.setText(contraseñad)
+
+
+            //quita el efectoclick
+            usuario.isEnabled = false
+            contraseña.isEnabled = false
+
+        }
+    }
+    private fun habilitaredicion() {
+
+        val usuario: EditText = findViewById(R.id.usuario)
+        val contraseña: EditText = findViewById(R.id.contraseña)
+
+        //pone el efectoclick
+        usuario.isEnabled = true
+        contraseña.isEnabled = true
+
+        val boton: Button = findViewById(R.id.button2)
+        boton.visibility = View.VISIBLE
+        val boton2: Button = findViewById(R.id.button3)
+        boton2.visibility = View.GONE
+
+    }
+
+    /*private fun generarUUIDv4(): String {
         val uuid = UUID.randomUUID()
         return uuid.toString().toUpperCase()
+    }*/
+    private fun guardarInformacion() {
+        val app = application as MyApp
+        val database = app.database
+        val usuario = usuario.text.toString()
+        val contraseña = contraseña.text.toString()
+
+        // Buscar si ya existe un documento del tipo "ConfEmisor"
+        val query = QueryBuilder.select(SelectResult.expression(Meta.id))
+            .from(DataSource.database(database))
+            .where(Expression.property("tipo").equalTo(Expression.string("Autentificacion")))
+
+        try {
+            val resultSet = query.execute()
+            val results = resultSet.allResults()
+
+            if (results.isNotEmpty()) {
+                // Iterar sobre los resultados y eliminar cada documento
+                for (result in results) {
+                    val docId = result.getString(0) // Obtenemos el ID del documento en el índice 0
+                    docId?.let {
+                        val document = database.getDocument(it)
+                        document?.let {
+                            database.delete(it)
+                        }
+                    }
+                }
+                Log.d("ReClienteActivity", "Documento existente borrado")
+            }
+
+            // Crear un nuevo documento
+            val document = MutableDocument()
+                .setString("usuario", usuario)
+                .setString("contraseña", contraseña)
+                .setString("tipo", "Autentificacion")
+
+            // Guardar el nuevo documento
+            database.save(document)
+            Log.d("ReClienteActivity", "Datos guardados correctamente: \n $document")
+            Toast.makeText(this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show()
+        } catch (e: CouchbaseLiteException) {
+            Log.e("ReClienteActivity", "Error al guardar los datos en la base de datos: ${e.message}", e)
+            Toast.makeText(this, "Error al guardar los datos", Toast.LENGTH_SHORT).show()
+        }
     }
+    private fun obtenerDatosGuardados(): List<String> {
+        // Obtén la instancia de la base de datos desde la aplicación
+        val app = application as MyApp
+        val database = app.database
+
+        // Crea una consulta para seleccionar todos los documentos con tipo = "cliente"
+        val query = QueryBuilder.select(SelectResult.all())
+            .from(DataSource.database(database))
+            .where(Expression.property("tipo").equalTo(Expression.string("Autentificacion")))
+
+        // Ejecuta la consulta
+        val result = query.execute()
+
+        // Lista para almacenar los datos obtenidos
+        val dataList = mutableListOf<String>()
+
+        // Itera sobre todos los resultados de la consulta
+        result.allResults().forEach { result ->
+            // Obtiene el diccionario del documento del resultado actual
+            val dict = result.getDictionary(database.name)
+
+            // Extrae los valores de los campos del documento
+            val usuario = dict?.getString("usuario")
+            val contraseña = dict?.getString("contraseña")
+
+            // Formatea los datos como una cadena y la agrega a la lista
+            val dataString = "$usuario\n$contraseña"
+            dataList.add(dataString)
+        }
+
+        // Devuelve la lista de datos
+        return dataList
+    }
+
 
 }
