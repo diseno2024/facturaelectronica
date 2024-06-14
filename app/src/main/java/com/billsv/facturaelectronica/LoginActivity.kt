3418c9b423838d8ae17c6ab7a3696d9bf2af7915
EmersonOrellana <ot20006@ues.edu.sv>
2024-06-14 13:28:34 -0600

    Reparado el login
    Ya pide el pin al instalar por primera vez

--- app/src/main/java/com/billsv/facturaelectronica/LoginActivity.kt
+++ app/src/main/java/com/billsv/facturaelectronica/LoginActivity.kt
@@ -1,4 +1,5 @@
 package com.billsv.facturaelectronica
+
 import android.Manifest
 import android.content.Context
 import android.content.DialogInterface
@@ -6,7 +7,6 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.text.InputFilter
 import android.text.SpannableString
@@ -24,7 +24,6 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import android.view.inputmethod.EditorInfo
 
 class LoginActivity : AppCompatActivity() {
 
@@ -32,17 +31,9 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var loginButton: Button
     private lateinit var linkTextView: TextView
 
-    private val PIN_FILE_NAME = "pins.json" // Nombre del archivo para guardar los pines
-    private val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 101
-    private val permissionList: List<String> = if (Build.VERSION.SDK_INT >= 33) {
-        listOf(
-            Manifest.permission.READ_MEDIA_AUDIO,
-            Manifest.permission.READ_MEDIA_VIDEO,
-            Manifest.permission.READ_MEDIA_IMAGES
-        )
-    } else {
-        listOf(
-            Manifest.permission.READ_EXTERNAL_STORAGE,
+    companion object {
+        private const val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 100
+        private val permissionList = listOf(
             Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
     }
@@ -51,43 +42,43 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // Inicializar vistas
         pinEditText = findViewById(R.id.pinEditText)
         loginButton = findViewById(R.id.loginButton)
         linkTextView = findViewById(R.id.linkTextView)
 
-        // Agregar un listener para la acción "Done" o "Enter" en el teclado
-        pinEditText.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                // Realizar la acción de inicio de sesión cuando se presiona "Done" o "Enter"
-                iniciarSesion()
-                return@setOnEditorActionListener true // Devolver true para indicar que se ha manejado la acción
+        // Verificar si es la primera vez que se instala la aplicación
+        val sharedPreferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        val isFirstTime = sharedPreferences.getBoolean("is_first_time", true)
+
+        if (isFirstTime) {
+            // Mostrar un cuadro de diálogo para crear un nuevo PIN
+            showCreatePinDialog()
+        } else {
+            // Verificar si hay un PIN guardado
+            val savedPins = loadPinsFromMenuActivity()
+            if (savedPins.isEmpty()) {
+                // Si no hay PIN guardado, mostrar mensaje para crear uno
+                Toast.makeText(this, "Por favor, crea un nuevo PIN", Toast.LENGTH_SHORT).show()
             }
-            false // Devolver false para indicar que la acción no se ha manejado completamente
         }
 
         // Configurar el botón de inicio de sesión
         loginButton.setOnClickListener {
-            iniciarSesion()
+            val enteredPin = pinEditText.text.toString()
+            val savedPins = loadPinsFromMenuActivity()
+            if (savedPins.contains(enteredPin)) {
+                // PIN correcto, iniciar MenuActivity
+                val intent = Intent(this, MenuActivity::class.java)
+                startActivity(intent)
+                requestPermissions()
+                finish() // Finalizar LoginActivity para evitar que el usuario regrese presionando el botón Atrás
+            } else {
+                // PIN incorrecto, mostrar mensaje de error
+                Toast.makeText(this, "PIN incorrecto", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        // Configurar el enlace clickable
         setupClickableLink()
-    }
-
-    private fun iniciarSesion() {
-        val enteredPin = pinEditText.text.toString()
-        val savedPins = loadPinsFromMenuActivity()
-        if (savedPins.contains(enteredPin)) {
-            // PIN correcto, iniciar MenuActivity
-            val intent = Intent(this@LoginActivity, MenuActivity::class.java)
-            startActivity(intent)
-            requestPermissions()
-            finish() // Finalizar LoginActivity para evitar que el usuario regrese presionando el botón Atrás
-        } else {
-            // PIN incorrecto, mostrar mensaje de error
-            Toast.makeText(this@LoginActivity, "PIN incorrecto", Toast.LENGTH_SHORT).show()
-        }
     }
 
     private fun showCreatePinDialog() {
@@ -95,21 +86,21 @@ class LoginActivity : AppCompatActivity() {
         dialogBuilder.setTitle("Crear PIN")
         dialogBuilder.setMessage("Por favor, crea un nuevo PIN (máximo 6 dígitos)")
         val input = EditText(this)
-        input.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(6)) // Máximo 6 dígitos
+        input.filters = arrayOf(InputFilter.LengthFilter(6)) // Máximo 6 dígitos
         input.inputType = android.text.InputType.TYPE_CLASS_NUMBER // Solo números
         dialogBuilder.setView(input)
 
-        dialogBuilder.setPositiveButton("Guardar", DialogInterface.OnClickListener { dialog, which ->
+        dialogBuilder.setPositiveButton("Guardar") { dialog, which ->
             val enteredPin = input.text.toString()
             savePin(enteredPin)
-            Toast.makeText(this@LoginActivity, "PIN creado correctamente", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "PIN creado correctamente", Toast.LENGTH_SHORT).show()
 
             // Guardar que ya no es la primera vez que se instala la aplicación
             val sharedPreferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
             val editor = sharedPreferences.edit()
             editor.putBoolean("is_first_time", false)
             editor.apply()
-        })
+        }
 
         dialogBuilder.setCancelable(false)
         dialogBuilder.show()
@@ -136,7 +127,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun setupClickableLink() {
-        val text = "Leer mas sobre nosotros aquí"
+        val text = "Leer más sobre nosotros aquí"
         val spannableString = SpannableString(text)
         val clickableSpan = object : ClickableSpan() {
             override fun onClick(widget: View) {
@@ -146,8 +137,8 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        val start = text.indexOf("Leer mas sobre nosotros aquí")
-        val end = start + "Leer mas sobre nosotros aquí".length
+        val start = text.indexOf("Leer más sobre nosotros aquí")
+        val end = start + "Leer más sobre nosotros aquí".length
         spannableString.setSpan(clickableSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
         linkTextView.text = spannableString
@@ -158,43 +149,23 @@ class LoginActivity : AppCompatActivity() {
         val permissionsToRequest = mutableListOf<String>()
 
         for (permission in permissionList) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    permission
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
                 permissionsToRequest.add(permission)
             }
         }
 
         if (permissionsToRequest.isNotEmpty()) {
-            ActivityCompat.requestPermissions(
-                this,
-                permissionsToRequest.toTypedArray(),
-                WRITE_EXTERNAL_STORAGE_REQUEST_CODE
-            )
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), WRITE_EXTERNAL_STORAGE_REQUEST_CODE)
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
-                Toast.makeText(
-                    this,
-                    "Permisos Concedidos",
-                    Toast.LENGTH_SHORT
-                ).show()
+                Toast.makeText(this, "Permisos Concedidos", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(
-                    this,
-                    "Los permisos son necesarios para realizar el respaldo",
-                    Toast.LENGTH_SHORT
-                ).show()
+                Toast.makeText(this, "Los permisos son necesarios para realizar el respaldo", Toast.LENGTH_SHORT).show()
             }
         }
     }
