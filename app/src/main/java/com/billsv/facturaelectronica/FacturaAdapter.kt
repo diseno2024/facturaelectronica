3cebe2976810a6105d415e06e9713f055152a6ad
BryanPortillo13 <pm18042@ues.edu.sv>
2024-06-15 22:31:22 -0600

    PAGINADO FACTURAS ( DE 3 EN 3)

--- app/src/main/java/com/billsv/facturaelectronica/FacturaAdapter.kt
+++ app/src/main/java/com/billsv/facturaelectronica/FacturaAdapter.kt
@@ -9,9 +9,11 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
+
+
 class FacturaAdapter(
     private val context: Context,
-    private val facturaList: List<Factura>
+    private var facturaList: MutableList<Factura>
 ) : RecyclerView.Adapter<FacturaAdapter.FacturaViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FacturaViewHolder {
@@ -21,22 +23,24 @@ class FacturaAdapter(
 
     override fun onBindViewHolder(holder: FacturaViewHolder, position: Int) {
         val factura = facturaList[position]
-        holder.textViewNombre.text = "${factura.nombre}"
+        holder.textViewNombre.text = factura.nombre
         holder.textViewTelefono.text = "Teléfono: ${factura.telefono}"
         holder.textViewDui.text = "DUI: ${factura.dui}"
-        // Puedes configurar otras propiedades aquí si es necesario
     }
 
     override fun getItemCount(): Int {
         return facturaList.size
     }
 
+    fun setFacturas(nuevasFacturas: List<Factura>) {
+        facturaList.clear()
+        facturaList.addAll(nuevasFacturas)
+        notifyDataSetChanged()
+    }
+
     class FacturaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textViewNombre: TextView = itemView.findViewById(R.id.textViewNombre)
         val textViewTelefono: TextView = itemView.findViewById(R.id.textViewTelefono)
         val textViewDui: TextView = itemView.findViewById(R.id.textViewDui)
-        val iconoCard: ImageView = itemView.findViewById(R.id.iconoCard)
-        val cardView: CardView = itemView.findViewById(R.id.card_factura)
     }
 }
-
--- app/src/main/java/com/billsv/facturaelectronica/FacturaFragment.kt
+++ app/src/main/java/com/billsv/facturaelectronica/FacturaFragment.kt
@@ -13,104 +13,121 @@ import com.couchbase.lite.QueryBuilder
 import com.couchbase.lite.SelectResult
 import com.couchbase.lite.DataSource
 import com.couchbase.lite.Dictionary
+import com.couchbase.lite.Ordering
 import com.couchbase.lite.Result
 import com.couchbase.lite.ResultSet
+
+import android.widget.Button
 
 
 class FacturaFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var facturaAdapter: FacturaAdapter
-    private lateinit var facturaList: List<Factura>
+    private lateinit var btnLoadMore: Button
+    private lateinit var btnLoadPrevious: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var currentPage = 0
+    private val pageSize = 3
+    private var currentData: List<Factura> = emptyList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_factura, container, false)
 
         recyclerView = view.findViewById(R.id.listRecycler)
         recyclerView.layoutManager = LinearLayoutManager(context)
 
-        // Obtener datos guardados
-        facturaList = obtenerDatosGuardados()
+        btnLoadMore = view.findViewById(R.id.btnLoadMore)
+        btnLoadMore.setOnClickListener {
+            loadMoreItems()
+        }
 
-        facturaAdapter = FacturaAdapter(requireContext(), facturaList)
+        btnLoadPrevious = view.findViewById(R.id.btnLoadPrevious)
+        btnLoadPrevious.setOnClickListener {
+            loadPreviousItems()
+        }
+
+        facturaAdapter = FacturaAdapter(requireContext(), mutableListOf())
         recyclerView.adapter = facturaAdapter
+
+        // Cargar la primera página de datos
+        loadMoreItems()
 
         return view
     }
 
-    private fun obtenerDatosGuardados(): List<Factura> {
-        // Obtén la instancia de la base de datos desde la aplicación
+    private fun loadMoreItems() {
+        val newData = obtenerDatosPaginados(currentPage * pageSize, pageSize)
+        if (newData.isNotEmpty()) {
+            currentData = newData
+            facturaAdapter.setFacturas(currentData)
+            currentPage++
+            updateButtonVisibility()
+        }
+    }
+
+    private fun loadPreviousItems() {
+        if (currentPage > 1) {
+            currentPage -= 2 // Retroceder dos páginas para cargar los datos anteriores
+            val previousData = obtenerDatosPaginados(currentPage * pageSize, pageSize)
+            currentData = previousData
+            facturaAdapter.setFacturas(currentData)
+            currentPage++
+            updateButtonVisibility()
+        }
+    }
+
+    private fun obtenerDatosPaginados(offset: Int, limit: Int): List<Factura> {
         val app = requireActivity().application as MyApp
         val database = app.database
 
-        // Crea una consulta para seleccionar todos los documentos con tipo = "cliente"
         val query = QueryBuilder.select(SelectResult.all())
             .from(DataSource.database(database))
             .where(Expression.property("tipo").equalTo(Expression.string("cliente")))
+            .limit(Expression.intValue(limit), Expression.intValue(offset))
 
-        // Ejecuta la consulta
         val result = query.execute()
-
-        // Lista para almacenar los datos obtenidos
         val dataList = mutableListOf<Factura>()
 
-        // Itera sobre todos los resultados de la consulta
         result.allResults().forEach { result ->
-            // Obtiene el diccionario del documento del resultado actual
             val dict = result.getDictionary(database.name)
-
-            // Extrae los valores de los campos del documento
             val nombre = dict?.getString("nombre") ?: ""
             val telefono = dict?.getString("telefono") ?: ""
             val dui = dict?.getString("dui") ?: ""
-
-            // Formatea el DUI con un guion antes del último dígito
             val duiFormateado = formatearDUI(dui)
-
-            // Formatea el número de teléfono con un guion después de los primeros 4 dígitos
             val telefonoFormateado = formatearTelefono(telefono)
-
-            // Crea una instancia de Factura y la agrega a la lista
             val factura = Factura(nombre, telefonoFormateado, duiFormateado)
             dataList.add(factura)
         }
 
-        // Devuelve la lista de datos
         return dataList
     }
 
-    // Función para formatear el DUI con un guion antes del último dígito
     private fun formatearDUI(dui: String): String {
         return if (dui.length >= 9) {
             val primerosDigitos = dui.substring(0, dui.length - 1)
             val ultimoDigito = dui.substring(dui.length - 1)
             "$primerosDigitos-$ultimoDigito"
         } else {
-            // Si el DUI no tiene suficientes dígitos, devolvemos el mismo DUI sin cambios
             dui
         }
     }
 
-    // Función para formatear el número de teléfono con un guion después de los primeros 4 dígitos
     private fun formatearTelefono(telefono: String): String {
         return if (telefono.length >= 8) {
             val primerosDigitos = telefono.substring(0, 4)
             val siguientesDigitos = telefono.substring(4)
             "$primerosDigitos-$siguientesDigitos"
         } else {
-            // Si el número de teléfono no tiene suficientes dígitos, devolvemos el mismo número sin cambios
             telefono
         }
     }
 
-
-
+    private fun updateButtonVisibility() {
+        btnLoadMore.visibility = if (currentData.size >= pageSize) View.VISIBLE else View.GONE
+        btnLoadPrevious.visibility = if (currentPage > 1) View.VISIBLE else View.GONE
+    }
 }
