724285734cf46060c23f6594b412bcf09103b89d
Gabym03 <100321923+Gabym03@users.noreply.github.com>
2024-06-18 00:24:24 -0600

    Consumo de API parte 2

---
+++ app/src/main/java/com/billsv/facturaelectronica/RetrofitClient.kt
@@ -0,0 +1,28 @@
+package com.billsv.facturaelectronica
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    //https://apitest.dtes.mh.gob.sv/seguridad/auth
+    //https://api.dtes.mh.gob.sv/seguridad/authw
+    private const val BASE_URL = ""
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val instance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient)
+            .build()
+    }
+}
