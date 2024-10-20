56f1f556a43da0626d93a9ffb3beb08e8c27686b
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-10-20 00:39:12 -0600

    Envío de PDF/JSON CF vía Gmail
    Ya se pueden enviar los documentos por medio de Gmail que se generen para FACTURAS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
-    implementation ("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation ("com.couchbase.lite:couchbase-lite-android-ktx:3.1.6")
     implementation("androidx.appcompat:appcompat:$appcompat_version")
     implementation("com.google.android.material:material:1.11.0")
